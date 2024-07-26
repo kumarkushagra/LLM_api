7234cdd47100a12c382638f2976d4930f6520539
kumarkushagra <kumarkushagra777@gmail.com>
2024-07-26 18:52:51 +0530

    Working text-to-audio

--- seamless M4T api/TxS.py
+++ seamless M4T api/TxS.py
@@ -1,11 +1,19 @@
-import requests
+from transformers import AutoProcessor, SeamlessM4TModel
+import scipy.io.wavfile
+from IPython.display import Audio
 
-def text_to_speech(text, src_lang, tgt_lang, api_token):
-    api_url = "https://api-inference.huggingface.co/models/facebook/seamless-m4t-v2-large"
-    headers = {"Authorization": f"Bearer {api_token}"}
-    payload = {
-        "inputs": text,
-        "parameters": {"src_lang": src_lang, "tgt_lang": tgt_lang, "generate_speech": True}
-    }
-    response = requests.post(api_url, headers=headers, json=payload)
-    return response.content  # This will be the audio array
+# processor = AutoProcessor.from_pretrained(r"F:\facebook\hf-seamless-m4t-large")
+# model = SeamlessM4TModel.from_pretrained(r"F:\facebook\hf-seamless-m4t-large")
+
+
+def text_to_speech(processor, model, input_text, input_lang, output_lang, output_path):
+    text_inputs = processor(text = input_text, src_lang=input_lang, return_tensors="pt")
+
+    audio_array_from_text = model.generate(**text_inputs, tgt_lang=output_lang)[0].cpu().numpy().squeeze()
+    # Define the sample rate from model config
+    sample_rate = model.config.sampling_rate
+    # Save the audio to a WAV file
+    scipy.io.wavfile.write(output_path, rate=sample_rate, data=audio_array_from_text)
+    # Play the audio
+    Audio(audio_array_from_text, rate=sample_rate)
+    return f"{output_path}/output1.mp3"
